b1d80e7b003234c10dd00ca53563158d74981a42
ra1nx92 <ra1nx1992@gmail.com>
2021-12-09 14:32:52 +0300

    completing the app

--- app/src/main/java/com/example/wheaterapp/data/service/WeatherApi.kt
+++ app/src/main/java/com/example/wheaterapp/data/service/WeatherApi.kt
@@ -3,8 +3,18 @@ package com.example.wheaterapp.data.service
 import com.example.wheaterapp.data.model.WeatherModel
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WeatherApi {
-    @GET("data/2.5/weather?q=izmir&appid=fd47fef64bf5ea7366df3e2b5de136ae")
-    fun getWeatherData():Single<WeatherModel>
+    @GET("data/2.5/weather?&units=metric&")
+    fun getWeatherData(
+        @Query("q")cityName:String = "",
+        @Query("appid")apiKey:String = API_KEY,
+        @Query("lang")language:String = LANGUAGE
+    ):Single<WeatherModel>
+
+    companion object{
+    private const val API_KEY = "fd47fef64bf5ea7366df3e2b5de136ae"
+    private const val LANGUAGE = "ru"
+    }
 }
--- app/src/main/java/com/example/wheaterapp/data/service/WeatherApiFactory.kt
+++ app/src/main/java/com/example/wheaterapp/data/service/WeatherApiFactory.kt
@@ -3,13 +3,12 @@ package com.example.wheaterapp.data.service
 import com.example.wheaterapp.data.model.WeatherModel
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import io.reactivex.rxjava3.core.Single
-import retrofit2.CallAdapter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object WeatherApiFactory {
 
-    const val BASE_URL = "api.openweathermap.org/"
+    private const val BASE_URL = "https://api.openweathermap.org/"
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
@@ -18,7 +17,7 @@ object WeatherApiFactory {
         .create(WeatherApi::class.java)
 
 
-    fun getDataService(): Single<WeatherModel>{
-        return retrofit.getWeatherData()
+    fun getDataService(cityName:String): Single<WeatherModel>{
+        return retrofit.getWeatherData(cityName)
     }
 }

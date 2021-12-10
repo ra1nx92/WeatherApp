97edb3721f70f70508e2c17b4861af01e6d0b2fa
ra1nx92 <ra1nx1992@gmail.com>
2021-12-10 11:15:11 +0300

    .

---
+++ app/src/main/java/com/example/wheaterapp/presentation/screen/MainActivity.kt
@@ -0,0 +1,114 @@
+package com.example.wheaterapp.data.presentation.screen
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.wheaterapp.data.presentation.viewModel.MainViewModel
+import com.example.wheaterapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var GET: SharedPreferences
+    private lateinit var SET: SharedPreferences.Editor
+
+    private val viewModel: MainViewModel by viewModels()
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        GET = getSharedPreferences(packageName, MODE_PRIVATE)
+        SET = GET.edit()
+
+        val cName = GET.getString("cityName", "ankara")
+        binding.edtCityName.setText(cName)
+
+        viewModel.refreshData(cName ?: "")
+        getLiveData()
+
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            swipeRefresh(cName)
+        }
+
+        binding.imgSearchCity.setOnClickListener {
+            clickSearchCity()
+        }
+    }
+
+    private fun swipeRefresh(cName: String?) {
+        with(binding) {
+            llData.visibility = View.GONE
+            tvError.visibility = View.GONE
+            pbLoading.visibility = View.GONE
+
+            val cityName = GET.getString("cityName", cName)
+            edtCityName.setText(cityName)
+            viewModel.refreshData(cityName ?: "")
+            swipeRefreshLayout.isRefreshing = false
+        }
+    }
+
+    private fun clickSearchCity() {
+        val cityName = binding.edtCityName.text.toString()
+        SET.putString("cityName", cityName)
+        SET.commit()
+        viewModel.refreshData(cityName)
+        getLiveData()
+    }
+
+    private fun getLiveData() {
+        viewModel.weatherData.observe(this, { data ->
+            data?.let {
+                with(binding) {
+                    llData.visibility = View.VISIBLE
+                    pbLoading.visibility = View.GONE
+                    tvDegree.text = data.main.temp.toString()
+                    tvCountryCode.text = data.sys.country
+                    tvCityName.text = data.name
+                    tvHumidity.text = data.main.humidity.toString()
+                    tvWindSpeed.text = data.wind.speed.toString()
+                    tvLat.text = data.coord.lat.toString()
+                    tvLon.text = data.coord.lon.toString()
+
+                    Glide.with(this@MainActivity)
+                        .load("https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
+                        .into(imgWeatherPictures)
+                }
+            }
+        })
+
+        viewModel.weatherLoad.observe(this, { load ->
+            load?.let {
+                with(binding) {
+                    if (it) {
+                        pbLoading.visibility = View.VISIBLE
+                        tvError.visibility = View.GONE
+                        llData.visibility = View.GONE
+                    } else {
+                        pbLoading.visibility = View.GONE
+                    }
+                }
+            }
+
+        })
+
+        viewModel.weatherError.observe(this, { error ->
+            error?.let {
+                with(binding) {
+                    if (it) {
+                        tvError.visibility = View.VISIBLE
+                        llData.visibility = View.GONE
+                        pbLoading.visibility = View.GONE
+                    } else {
+                        tvError.visibility = View.GONE
+                    }
+                }
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/example/wheaterapp/presentation/viewModel/MainViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.wheaterapp.data.presentation.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.wheaterapp.data.model.WeatherModel
+import com.example.wheaterapp.data.service.WeatherApiFactory
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.observers.DisposableSingleObserver
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class MainViewModel : ViewModel() {
+
+    private val disposable = CompositeDisposable()
+
+    val weatherData = MutableLiveData<WeatherModel>()
+    val weatherError = MutableLiveData<Boolean>()
+    val weatherLoad = MutableLiveData<Boolean>()
+
+    fun refreshData(cityName:String) {
+        getDataFromApi(cityName)
+    }
+
+
+    private fun getDataFromApi(cityName:String) {
+        weatherLoad.value = true
+        disposable.add(
+            WeatherApiFactory.getDataService(cityName)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<WeatherModel>() {
+                    override fun onSuccess(t: WeatherModel) {
+                        weatherData.value = t
+                        weatherError.value = false
+                    }
+
+                    override fun onError(e: Throwable) {
+                        weatherError.value = true
+                        weatherLoad.value = false
+                    }
+                })
+        )
+    }
+}
